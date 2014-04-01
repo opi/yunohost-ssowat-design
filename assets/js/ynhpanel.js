ee9845977711a67ce23e47b42ae5d0bb6cc95177
Thomas LEBEAU <thomas.lebeau@g-nova.fr>
2014-04-01 21:17:23 +0200

    fonts base64

--- assets/js/ynhpanel.js
+++ assets/js/ynhpanel.js
@@ -123,20 +123,17 @@ domReady(function(){
   if (window.self !== window.top) {return false;}
 
   // Add portal stylesheet
-
-  var font = document.createElement("link");
-  font.setAttribute("rel", "stylesheet");
-  font.setAttribute("type", "text/css");
-  font.setAttribute("href", '/fonts.css');
-  document.getElementsByTagName("head")[0].insertBefore(font, null);
-
   var portalStyle = document.createElement("link");
   portalStyle.setAttribute("rel", "stylesheet");
   portalStyle.setAttribute("type", "text/css");
   portalStyle.setAttribute("href", '/ynhpanel.css');
   document.getElementsByTagName("head")[0].insertBefore(portalStyle, null);
 
-  
+  var font = document.createElement("link");
+  font.setAttribute("rel", "stylesheet");
+  font.setAttribute("type", "text/css");
+  font.setAttribute("href", '/fonts.css');
+  document.getElementsByTagName("head")[0].insertBefore(font, null);
 
   // Create portal link
   var portal = document.createElement('a');
