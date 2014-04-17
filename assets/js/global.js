4971b258f2e9428bfd743422224fb0bc4fa16bef
Thomas LEBEAU <thomas.lebeau@g-nova.fr>
2014-04-17 06:42:43 +0200

    split name apps

--- assets/js/global.js
+++ assets/js/global.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
   var liMenu = document.querySelectorAll('#apps a'),
       colors = ['bluebg','purplebg','redbg','orangebg','greenbg','darkbluebg','lightbluebg','yellowbg','lightpinkbg'];
   [].forEach.call(liMenu, function(el, i) {
+    var text = el.textContent;
+    console.log(text);
     el.classList.add(colors[i]);
   });
 });
