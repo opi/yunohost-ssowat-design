28f74eb3795c24c31c7561686c30ffbbf4fa7cf4
Thomas LEBEAU <thomas.lebeau@g-nova.fr>
2014-04-17 06:36:23 +0200

    add color home panel

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-  
+  var liMenu = document.querySelectorAll('#apps li'),
+      colors = ['bluebg','purplebg','redbg','orangebg','greenbg','darkbluebg','lightbluebg','yellowbg','lightpinkbg'];
+  [].forEach.call(liMenu, function(el, i) {
+    el.classList.add(colors[i]);
+  });
 });
