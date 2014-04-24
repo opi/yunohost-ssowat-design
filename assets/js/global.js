a9151529bbc04850c559cf12b18dc1f9d8b8e309
Thomas LEBEAU <thomas.lebeau@g-nova.fr>
2014-04-24 13:41:18 +0200

    form edit

--- assets/js/global.js
+++ assets/js/global.js
@@ -16,11 +16,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
   addMailAlias.addEventListener('click', function(){
     var inputAlias = document.querySelector('.mailalias-input');
-    formMailAlias.insertBefore(inputAlias.cloneNode(true)+'</br></br>', null);
+    formMailAlias.insertBefore(inputAlias.cloneNode(true), null);
   });
 
   addMaildrop.addEventListener('click', function(){
     var inputDrop = document.querySelector('.maildrop-input');
-    formMailDrop.insertBefore(inputDrop.cloneNode(true)+'</br></br>', null);
+    formMailDrop.insertBefore(inputDrop.cloneNode(true), null);
   });
 });
