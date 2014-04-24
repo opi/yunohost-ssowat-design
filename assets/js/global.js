d38df93a95f87022ad1a42518a36c473b1ca0b97
Thomas LEBEAU <thomas.lebeau@g-nova.fr>
2014-04-24 07:01:03 +0200

    form edit

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,11 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
   var liMenu = document.querySelectorAll('#apps a'),
-      colors = ['bluebg','purplebg','redbg','orangebg','greenbg','darkbluebg','lightbluebg','yellowbg','lightpinkbg'];
+      colors = ['bluebg','purplebg','redbg','orangebg','greenbg','darkbluebg','lightbluebg','yellowbg','lightpinkbg'],
+      addMailAlias = document.getElementById('add-mailalias'),
+      addMaildrop = document.getElementById('add-maildrop');
   [].forEach.call(liMenu, function(el, i) {
     var text = el.textContent,
         splitText = text.split("");
 
     el.classList.add(colors[i]);
     el.querySelector('.first-letter').setAttribute('data-first-letter',splitText[0]+splitText[1]);
+  });
+
+  addMailAlias.addEventListener('click', function(){
+    var inputAlias = document.querySelector('.mailalias-input:first');
+    this.insertBefore(inputAlias.cloneNode(true), null);
+  });
+
+  addMaildrop.addEventListener('click', function(){
+    var inputDrop = document.querySelector('.maildrop-input:first');
+    this.insertBefore(inputDrop.cloneNode(true), null);
   });
 });
